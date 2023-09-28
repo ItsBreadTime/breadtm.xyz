e858584c5b5a50bb91a3cb2dbe85e775f9be98ee
ItsBreadTime <89189459+ItsBreadTime@users.noreply.github.com>
2023-09-28 18:47:04 +0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<div
+  class="h-screen flex flex-col items-center justify-center tracking-widest text-gray-300 px-4 lg:px-0"
+>
+  <img
+    src="pfp.png"
+    alt="Bread"
+    class="p-4 bg-black m-8 w-32 h-32 mx-auto rounded-lg"
+  />
+  <h1
+    class="font-mono text-2xl lg:text-4xl font-bold tracking-wider w-full text-center mb-6"
+  >
+    Hi! I'm <span class="bg-black p-2 rounded rotate-12">Bread</span>
+  </h1>
+  <p class="mb-4 hidden md:block">
+    A <span class="font-semibold">random</span> person using the internet™️
+  </p>
+
+  <div
+    class="p-4 lg:p-10 rounded-lg bg-gray-700 mt-4 text-gray-300 w-full lg:w-3/4 mx-auto"
+  />
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.indigo.900);
+  }
+</style>
