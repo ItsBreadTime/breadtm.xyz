d70c872bf6c8c530f8f112f934329660a9587eb5
ItsBreadTime <89189459+ItsBreadTime@users.noreply.github.com>
2025-09-09 13:25:57 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,8 @@
   import Nav from "./sections/Nav.svelte";
   import Header from "./sections/Header.svelte";
   import Contact from "./sections/Contact.svelte";
-  import Projects from "./sections/Projects.svelte";
 </script>
 
 <Nav />
 <Header />
-<Projects />
 <Contact />
